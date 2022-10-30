cb77d156dbdda08c69db449f2807a378ac1cae79
Mariano Jeanneret <marjeanneret1@gmail.com>
2022-10-30 18:43:19 -0300

    se resolvió la implementación del storage correctamente aunque la recuperación se da sobre la variable del array declarada como let en lugar de const. Resta resolver el conflicto con el Else a fin de que no haya que refrescar la página para poder volver a consultar mediante legajos ya ingresados

--- 3erPreentrega.js
+++ 3erPreentrega.js
@@ -9,13 +9,17 @@ class Empleado {
   }
 }
 
-const nominaEmpleados = [];
-nominaEmpleados.push(new Empleado(1, "PEDRO", 22, 10));
-nominaEmpleados.push(new Empleado(2, "JUAN", 18, 15));
-nominaEmpleados.push(new Empleado(3, "LUCAS", 16, 8));
-nominaEmpleados.push(new Empleado(4, "MARIA", 18, 10));
-nominaEmpleados.push(new Empleado(5, "ISABEL", 15, 12));
-nominaEmpleados.push(new Empleado(6, "LUCIA", 22, 20));
+// SE DECLARO EL ARRAY CON UN LET A FIN DE RECUPERAR EN LA MISMA VARIABLE LA SESION DE STORAGE.
+let nominaEmpleados = [];
+
+obtener_localstorage();
+
+function obtener_localstorage() {
+  if (localStorage.getItem("nominaGuardada")) {
+    nominaEmpleados = JSON.parse(localStorage.getItem("nominaGuardada"));
+  }
+}
+
 console.table(nominaEmpleados);
 
 let datoSolicitado = document.querySelector("div#ingresoDeDatos");
@@ -33,42 +37,41 @@ datoSolicitado.innerHTML += `
 let legajoIngresado;
 
 let btnConsultar = document.getElementById("btnConsultar");
-btnConsultar.addEventListener("click", ()=> {
-  legajoIngresado = document.getElementById ("inputLegajo").value;
-  realizarConsulta(legajoIngresado)})  
+btnConsultar.addEventListener("click", () => {
+  legajoIngresado = document.getElementById("inputLegajo").value;
+  realizarConsulta(legajoIngresado);
+});
 
 let btnLimpiar = document.getElementById("btnLimpiar");
-btnLimpiar.addEventListener("click", ()=> {
-  document.getElementById("inputLegajo").value="";
+btnLimpiar.addEventListener("click", () => {
+  document.getElementById("inputLegajo").value = "";
   document.querySelector("div#datosParaConsulta").textContent = "";
   document.querySelector("div#reciboDeSueldo").textContent = "";
-  }
-  )
-debugger
-function realizarConsulta(){
-  
-    const empleadoEnNomina = nominaEmpleados.find(
-      (empl) => empl.legajo == legajoIngresado
-      );
-      
-      if (empleadoEnNomina) {
-        consulta++;
-        liquidar(
-          empleadoEnNomina.nombre,
-          empleadoEnNomina.legajo,
-          empleadoEnNomina.categoria,
-          empleadoEnNomina.antiguedad,
-          consulta,
-          );
-          
-        } else {
-          
-          let remove = document.getElementById("remover");
-          remove.remove();
-          document.querySelector("div#datosParaConsulta").textContent = "";
-          document.querySelector("div#reciboDeSueldo").textContent = "";
-    
-          datoSolicitado.innerHTML += `
+});
+
+function realizarConsulta() {
+  const empleadoEnNomina = nominaEmpleados.find(
+    (empl) => empl.legajo == legajoIngresado
+  );
+
+  if (empleadoEnNomina) {
+    consulta++;
+    liquidar(
+      empleadoEnNomina.nombre,
+      empleadoEnNomina.legajo,
+      empleadoEnNomina.categoria,
+      empleadoEnNomina.antiguedad,
+      consulta
+    );
+
+    // AL INGRESAR AL SIGUIENTE ELSE CUANDO EL LEGAJO NO SE ENCUENTRA REGISTRADO NO SE PUEDE VOLVER A CONSULTAR SOLO MEDIANTE EL INGRESO DE UN LEGAJO SI NO SE REFRESCA LA PAGINA.
+  } else {
+    let remove = document.getElementById("remover");
+    remove.remove();
+    document.querySelector("div#datosParaConsulta").textContent = "";
+    document.querySelector("div#reciboDeSueldo").textContent = "";
+
+    datoSolicitado.innerHTML += `
                     
             <h4> No tenemos registros bajo el legajo Nro ${legajoIngresado}, le solicitamos los siguientes datos para practicar la liquidación </h4>
             
@@ -82,62 +85,60 @@ function realizarConsulta(){
               <button class="btn btn-info" id="btnNuevaConsulta">Consultar</button>
               <button class="btn btn-warning" id="btnLimpiarTodo">Limpiar</button>
             </div>
-            `
-          
-         let btnNuevaConsulta = document.getElementById("btnNuevaConsulta");
-         btnNuevaConsulta.addEventListener("click", ()=> {
-           consulta++;
-           legajoIngresado;
-           let nombre = document.getElementById("inputNombre").value;
-           let categoria = document.getElementById("inputCategoria").value;
-           let antiguedad = document.getElementById("inputAntiguedad").value;
-           liquidar(nombre, legajoIngresado, categoria, antiguedad, consulta);
-           nominaEmpleados.push(
-            new Empleado(legajoIngresado, nombre, categoria, antiguedad)
-            );
-            console.clear();
-            console.table(nominaEmpleados);
-          });
-          
-          let btnLimpiarTodo = document.getElementById("btnLimpiarTodo");
-            btnLimpiarTodo.addEventListener("click", () => {
-            document.getElementById("inputNombre").value = "";
-            document.getElementById("inputCategoria").value ="";
-            document.getElementById("inputAntiguedad").value ="";
-            document.querySelector("div#datosParaConsulta").textContent = "";
-            document.querySelector("div#reciboDeSueldo").textContent = "";
-        });
-        
+            `;
+
+    let btnNuevaConsulta = document.getElementById("btnNuevaConsulta");
+    btnNuevaConsulta.addEventListener("click", () => {
+      consulta++;
+      legajoIngresado;
+      let nombre = document.getElementById("inputNombre").value;
+      let categoria = document.getElementById("inputCategoria").value;
+      let antiguedad = document.getElementById("inputAntiguedad").value;
+      liquidar(nombre, legajoIngresado, categoria, antiguedad, consulta);
+      nominaEmpleados.push(
+        new Empleado(legajoIngresado, nombre, categoria, antiguedad)
+      );
+      console.clear();
+      console.table(nominaEmpleados);
+
+      guardar_localstorage();
+
+      function guardar_localstorage() {
+        nominaEmpleados;
+        localStorage.setItem("nominaGuardada", JSON.stringify(nominaEmpleados));
       }
-          
-              
-              // if (isNaN(categoria) || isNaN(antiguedad) || nombre === "") {
-              //   alert(
-              //     "la categoria y antiguedad deben ser consignadas en número y el nombre no puede estar vacio"
-              //     );
-                
-            }            
-                  // numeroConsulta(consulta);
+    });
+
+    let btnLimpiarTodo = document.getElementById("btnLimpiarTodo");
+    btnLimpiarTodo.addEventListener("click", () => {
+      document.getElementById("inputNombre").value = "";
+      document.getElementById("inputCategoria").value = "";
+      document.getElementById("inputAntiguedad").value = "";
+      document.querySelector("div#datosParaConsulta").textContent = "";
+      document.querySelector("div#reciboDeSueldo").textContent = "";
+    });
+  }
+}
+
+// CUANDO SE RESUELVA LA SALIDA DEL ELSE SIN REFRESCAR LA PAGINA SE PUEDE AGREGAR ESTA ESTRUCTRA DE CONTROL
+// if (isNaN(categoria) || isNaN(antiguedad) || nombre === "") {
+//   alert(
+//     "la categoria y antiguedad deben ser consignadas en número y el nombre no puede estar vacio"
+//     );
 
 function liquidar(nombre, legajo, categoria, antiguedad, consulta) {
-    let datos = document.querySelector("div#datosParaConsulta")
-    datos.innerHTML = 
-    `<h3>Hola ${nombre}!</h3>
+  let datos = document.querySelector("div#datosParaConsulta");
+  datos.innerHTML = `<h3>Hola ${nombre}!</h3>
       <p>se ralizará la consulta solicitada al legajo Nro: ${legajo}</p>
       <p>en base a una categoria Nro: ${categoria}</p>
       <p>con una antiguedad de ${antiguedad} años</p>
-      <p>Consulta numero: ${consulta}`
+      <p>Consulta numero: ${consulta}`;
   const itemUrSueldo = 10000 * categoria;
   const itemAntiguedad = 1000 * antiguedad;
   const sueldo = itemUrSueldo + itemAntiguedad;
-  let recibo = document.querySelector("div#reciboDeSueldo")
-  recibo.innerHTML = 
-      `<h3>Su sueldo asciende a $ ${sueldo}</h3> 
+  let recibo = document.querySelector("div#reciboDeSueldo");
+  recibo.innerHTML = `<h3>Su sueldo asciende a $ ${sueldo}</h3> 
       <p>el cual esta conformado por: </p>
       <h4>$ ${itemUrSueldo} de sueldo y </h4>
-      <h4>$ ${itemAntiguedad} de antiguedad.</h4>`
+      <h4>$ ${itemAntiguedad} de antiguedad.</h4>`;
 }
-
-// function numeroConsulta(consulta) {
-//   alert("Consultas realizadas en esta sesión: " + consulta + "\nHasta pronto!");
-// }
